8bb017c95d38f605c217043b7fbf794356284afb
Al Sahriyar <alsahriyar229@gmail.com>
2026-01-13 22:57:14 +0600

    Next js

---
+++ src/app/contact/branch/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function BranchPage() {
+  return (
+    <div>BranchPage</div>
+  )
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+
+
+export default function signup() {
+  return (
+    <div>singup</div>
+  )
+}
